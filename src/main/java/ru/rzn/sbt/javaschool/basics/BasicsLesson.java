9e44132b8c7f3acdb331335f8efc8728aafb6203
AnnaDante1 <mysterrious_16@mail.ru>
2019-01-08 18:36:09 +0400

    LethalWeapon

--- src/main/java/ru/rzn/sbt/javaschool/basics/BasicsLesson.java
+++ src/main/java/ru/rzn/sbt/javaschool/basics/BasicsLesson.java
@@ -69,6 +69,13 @@ public class BasicsLesson {
     }
 
 
+    //  {
+    //       log.log("From " + x + " to 1" + "\n");
+    //   }
+    //    }
+    //  log.log(x + " " + " little indian" + (x>1 ? "s": "") + "\n");
+
+
     /**
      * Всё в лог.<br/>
      * <br/>
@@ -79,14 +86,10 @@ public class BasicsLesson {
      */
     public void logEverything(StringSupplier supplier, Logger log) {
 
-        while (supplier.hasNext() == true) {
+        while (supplier.hasNext()) {
             log.log(supplier.next());
-            if (supplier.hasNext() == false) {
-                break;
-            }
         }
     }
-
 
     /**
      * Устами младенца.<br/>
@@ -118,120 +121,101 @@ public class BasicsLesson {
      * </ul>
      * Создайте объект нового класса, проинициализировав все его поля и верните его из метода {@link #newClass()}.<br/>
      */
-    public class LethalWeapon {
-        public String color;
-        private int roundsLeft;
-        private Double power;
 
-        public void reload(int i) {
-            //  i = 1;
-            for (i = 1; i > 0; i++) {
-                System.out.println(i + roundsLeft);
+        public Object newClass() {
+            Object o = new LethalWeapon("blue",1.5,1);
+            return o;
+        }
+
+        /**
+         * Добавьте в класс LethalWeapon серийные номера.<br />
+         * <br />
+         * Создайте приватное статическое целочисленное ({@code long}) поле nextSerial = 0.<br />
+         * Создайте приватное финальное целочисленное ({@code long}) поле serial с методом чтения.<br />
+         * В конструкторах обеспечьте корректное проставление серийных номеров.<br />
+         */
+        public void serial() {
+        }
+
+        /**
+         * Реализуйте метод equals() у класса LethalWeapon. Сравните цвет, мощность и заряд. Серийный номер роли не играет.
+         */
+        public void equalsContract() {
+
+        }
+
+        /**
+         * Получив некоторую строку, получите из нее подстроку со 2-го символа по символ "S",
+         * приведите все символы в верхний регистр и верните результат.
+         *
+         * @param input
+         * @return
+         */
+        public String stringExercise1(String input) {
+            input = input.substring(2, input.indexOf("S") + 1).toUpperCase();
+            System.out.println(input);
+            return input;
+        }
+
+        /**
+         * Получив некоторую строку, найдите длину строки, количество запятых, количество дефисов,
+         * индекс первого символа "R", индекс последнего символа "L" и количество пробелолв между словами.
+         * Верните результат перемножения всех полученных чисел.
+         *
+         * @param input
+         * @return
+         */
+
+        public int stringExercise2(String input) {
+            int x = input.trim().replaceAll("[^ ]", "").length();
+            int y = input.length() - input.replaceAll("[,]", "").length();
+            int z = input.length() - input.replaceAll("[-]", "").length();
+            int len = input.length();
+            int i = input.length() * (input.indexOf("R")) * (input.lastIndexOf("L")) * x * y * z;
+            System.out.println(i);
+            return i;
+        }
+
+
+        /**
+         * Получив некоторую строку выполните следующее:<br />
+         * 1. если в строке 3 слова и есть слово "Hello", верните новую строку, содержащую первое и последнее слова (без лишних пробелов).<br />
+         * 2. если в строке 2 слова и есть слово "Hello", инвертируйте порядок символов в ней и верните результат.<br />
+         * 3. если в строке 3 слова и есть слово "Goodbye", верните новую строку, содержащую первое и последнее слова (без лишних пробелов),
+         * если в конце есть восклицательный знак - уберите его, если нет - добавьте.<br />
+         * 4. если в строке 2 слова и есть слово "Goodbye", поменяйте слова местами (между словами в новой строке тоже должен быть пробел, лишних пробелов быть не должно),
+         * сделайте все буквы в нижнем регистре и верните результат.
+         *
+         * @param input
+         * @return
+         */
+        public String stringExercise3(String input) {
+            int wordCount = input.trim().split(" ").length;
+            input = input.trim();
+            while (input.contains("Hello")) {
+                if (wordCount == 2) {
+                    String builder = new StringBuilder(input).reverse().toString();
+                    System.out.println(builder);
+                    return builder;
+                } else {
+                    String builder1 = new StringBuilder(input).delete(input.indexOf("w"), input.lastIndexOf("l") - 3).toString();
+                    System.out.println(builder1.trim());
+                    return builder1.trim();
+                }
+            }
+            if (wordCount == 2) {
+                String[] strings = input.split(" ");
+                StringBuilder builder1 = new StringBuilder();
+                for (int i = strings.length - 1; i >= 0; i--) {
+                    builder1.append(strings[i] + " ");
+                }
+                System.out.println(builder1.toString().trim().toLowerCase());
+                return builder1.toString().trim().toLowerCase();
+            } else {
+                String builder1 = new StringBuilder(input).delete(input.lastIndexOf("c"), input.lastIndexOf("l") - 3).toString();
+                builder1 = input.contains("world!") ? (builder1.replaceAll("[!]", "")) : (builder1 + "!");
+                System.out.println(builder1);
+                return builder1;
             }
         }
-
-        public void pewPew() {
-
-        }
-
-        public LethalWeapon() {
-
-        }
-
-        LethalWeapon LethalWeaponNew = new LethalWeapon();
-
-        {
-            this.color = color;
-            this.roundsLeft = roundsLeft;
-            this.power = power;
-        }
     }
-
-    public Object newClass() {
-        String color;
-        int roundsLeft;
-        Double power;
-
-        return "";
-
-    }
-
-    /**
-     * Добавьте в класс LethalWeapon серийные номера.<br />
-     * <br />
-     * Создайте приватное статическое целочисленное ({@code long}) поле nextSerial = 0.<br />
-     * Создайте приватное финальное целочисленное ({@code long}) поле serial с методом чтения.<br />
-     * В конструкторах обеспечьте корректное проставление серийных номеров.<br />
-     */
-    public void serial() {
-    }
-
-    /**
-     * Реализуйте метод equals() у класса LethalWeapon. Сравните цвет, мощность и заряд. Серийный номер роли не играет.
-     */
-    public void equalsContract() {
-
-    }
-
-    /**
-     * Получив некоторую строку, получите из нее подстроку со 2-го символа по символ "S",
-     * приведите все символы в верхний регистр и верните результат.
-     *
-     * @param input
-     * @return
-     */
-    public String stringExercise1(String input) {
-        input = input.substring(2, input.indexOf("S") + 1).toUpperCase();
-        System.out.println(input);
-        return input;
-    }
-
-    /**
-     * Получив некоторую строку, найдите длину строки, количество запятых, количество дефисов,
-     * индекс первого символа "R", индекс последнего символа "L" и количество пробелолв между словами.
-     * Верните результат перемножения всех полученных чисел.
-     *
-     * @param input
-     * @return
-     */
-
-    public int stringExercise2(String input) {
-        int x = input.trim().replaceAll("[^ ]", "").length();
-        int y = input.length() - input.replaceAll("[,]", "").length();
-        int z = input.length() - input.replaceAll("[-]", "").length();
-        int len = input.length();
-        int i = input.length() * (input.indexOf("R")) * (input.lastIndexOf("L")) * x * y * z;
-        System.out.println(i);
-        return i;
-    }
-
-
-    /**
-     * Получив некоторую строку выполните следующее:<br />
-     * 1. если в строке 3 слова и есть слово "Hello", верните новую строку, содержащую первое и последнее слова (без лишних пробелов).<br />
-     * 2. если в строке 2 слова и есть слово "Hello", инвертируйте порядок символов в ней и верните результат.<br />
-     * 3. если в строке 3 слова и есть слово "Goodbye", верните новую строку, содержащую первое и последнее слова (без лишних пробелов),
-     * если в конце есть восклицательный знак - уберите его, если нет - добавьте.<br />
-     * 4. если в строке 2 слова и есть слово "Goodbye", поменяйте слова местами (между словами в новой строке тоже должен быть пробел, лишних пробелов быть не должно),
-     * сделайте все буквы в нижнем регистре и верните результат.
-     *
-     * @param input
-     * @return
-     */
-    public String stringExercise3(String input) {
-        int wordCount = input.split(" ").length;
-
-        if (wordCount == 2 || input.contains("Hello")) {
-            String buffer;
-            buffer = new StringBuffer(input).reverse().toString();
-
-            System.out.println(buffer);
-            return buffer;
-        } else if (wordCount == 3 || input.contains("Goodbye")) {
-
-        }
-
-        return input;
-    }
-}
-
